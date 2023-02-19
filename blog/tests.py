0d6ce38379f4e342206f2edf6f5a2b2a604326b9
sh0311 <0311alice@naver.com>
2023-02-19 16:50:13 +0900

    네비게이션바와 푸터 모듈화하기

--- blog/tests.py
+++ blog/tests.py
@@ -7,6 +7,22 @@ class TestView(TestCase):
     def setUp(self):
         self.client=Client()
 
+    def navbar_test(self, soup):
+        navbar=soup.nav
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About Me', navbar.text)
+
+        logo_btn=navbar.find('a', text='Alicesh')
+        self.assertEqual(logo_btn.attrs['href'], '/')
+
+        home_btn=navbar.find('a', text='Home')
+        self.assertEqual(home_btn.attrs['href'], '/')
+
+        blog_btn=navbar.find('a', text='Blog')
+        self.assertEqual(blog_btn.attrs['href'], '/blog/')
+
+        about_me_btn=navbar.find('a', text='About Me')
+        self.assertEqual(about_me_btn.attrs['href'], '/about_me/')
     def test_post_list(self):
         response=self.client.get('/blog/')
 
@@ -15,10 +31,9 @@ class TestView(TestCase):
         soup=BeautifulSoup(response.content, 'html.parser')
         self.assertEqual(soup.title.text, '남승현의 홈페이지')
 
-        navbar=soup.nav
 
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About Me', navbar.text)
+
+        self.navbar_test(soup)
 
         self.assertEqual(Post.objects.count(), 0)
 
